99bdbfe08635d51eae100612534fd7b8b9290ddd
Joaquin Lumelsky <jlumelsky@gmail.com>
2024-03-01 16:38:20 -0300

    configure allowed hosts for render

--- final_project/settings.py
+++ final_project/settings.py
@@ -35,7 +35,7 @@ DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 if os.getenv("DEVELOPMENT"):
     ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 else:
-    ALLOWED_HOSTS = ["tictactoe.lumel.dev", "tictactoe-m1zs.onrender.com"]
+    ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 
 
 # Application definition
