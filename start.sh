1ab8bf8b0e7da7ebd7a2530ad58664a5c4f8aba4
Joaquin Lumelsky <jlumelsky@gmail.com>
2024-03-01 14:27:12 -0300

    set 443 as daphne port

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 # exit on error
 set -o errexit
 gunicorn -b 0.0.0.0:10000 final_project.wsgi:application &
-daphne -b 0.0.0.0 -p 8000 final_project.asgi:application
+daphne -b 0.0.0.0 -p 443 final_project.asgi:application
